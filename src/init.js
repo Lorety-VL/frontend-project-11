407fcac1b4859df753913eb3f4279bdc22f41c03
Lorety-VL <vladimirking18@gmail.com>
2024-02-27 15:12:15 +0300

    add input validation

--- src/init.js
+++ src/init.js
@@ -1,32 +1,54 @@
 import * as yup from 'yup';
 import onChange from 'on-change';
 
-const validate = (state) => {
-  const schema = yup.string().required().url().notOneOf(state.usedRss);
-  return schema.isValid(state.inputValue);
+const validate = (state, inputVal) => {
+  const schema = yup.string().required().url('Ссылка должна быть валидным URL').notOneOf(state.usedRss, 'RSS уже существует');
+  return schema.validate(inputVal);
+};
+
+const renderMessage = (state, container) => {
+  container.textContent = state.message;
+};
+
+const renderInput = (state, input) => {
+  if (state.formState === 'valid') {
+    input.classList.remove('is-invalid');
+  }
+  if (state.formState === 'invalid') {
+    input.classList.add('is-invalid');
+  }
 };
 
 export default () => {
   const state = {
     formState: 'filling',
-    inputValue: '',
+    message: '',
     usedRss: [],
   };
 
   const form = document.querySelector('form');
   const input = form.elements.url;
-  const schema = yup.string().required().url().notOneOf(state.usedRss);
+  const messageContainer = document.querySelector('.feedback');
 
-  const wathedState = onChange(state, (_path, value) => {
-    
+  const wathedState = onChange(state, (path) => {
+    if (path === 'formState') {
+      renderInput(state, input);
+    }
+    if (path === 'message') {
+      renderMessage(state, messageContainer);
+    }
   });
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    state.inputValue = input.value;
-    validate(state)
-      .then((result) => {
-        if (result) state.usedRss.push(input.value);
+    validate(state, input.value)
+      .then(() => {
+        state.usedRss.push(input.value);
+        wathedState.formState = 'valid';
+        wathedState.message = '';
+      }).catch((err) => {
+        wathedState.message = err.message;
+        wathedState.formState = 'invalid';
       });
   });
 };
